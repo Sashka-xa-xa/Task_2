10665de75a45c4ba5128f8bd760d2af209d4ca59
Asash <sasha.kurbatova.88@mail.ru>
2023-04-24 13:53:02 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAmount = 100; // initial amount of money in the account
+        int addedAmount = 1100; // amount of money being added to the account
+        int bonus = 0; // bonus amount
+
+        if (addedAmount > 1000) {
+            bonus = addedAmount  / 100; // calculate bonus amount
+        }
+
+        int totalAmount = currentAmount + addedAmount + bonus; // calculate total amount
+
+        System.out.println("Total amount: " + totalAmount);
+        System.out.println("Bonus amount: " + bonus);
+    }
+}
+
